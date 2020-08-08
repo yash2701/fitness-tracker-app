42159c9f049396b9956f2baa728021a1306b4cfb
yash2701 <yash27095@gmail.com>
2020-08-08 16:37:39 -0400

    add a Dropdown menu in new-training.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -12,6 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     imports: [
@@ -27,7 +28,8 @@ import { MatCardModule } from '@angular/material/card';
         MatToolbarModule,
         MatListModule,
         MatTabsModule,
-        MatCardModule
+        MatCardModule,
+        MatSelectModule
     ],
     exports: [
         MatButtonModule,
@@ -41,7 +43,8 @@ import { MatCardModule } from '@angular/material/card';
         MatToolbarModule,
         MatListModule,
         MatTabsModule,
-        MatCardModule
+        MatCardModule,
+        MatSelectModule
     ]
 })
 export class MaterialModule{}
