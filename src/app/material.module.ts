0b340b4d1fbeed7bc6c7ae270c3c827ad01893da
yash2701 <yash27095@gmail.com>
2020-08-08 16:24:01 -0400

    add mat-card to new training and module.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,6 +11,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
     imports: [
@@ -25,7 +26,8 @@ import { MatTabsModule } from '@angular/material/tabs';
         MatSidenavModule,
         MatToolbarModule,
         MatListModule,
-        MatTabsModule
+        MatTabsModule,
+        MatCardModule
     ],
     exports: [
         MatButtonModule,
@@ -38,7 +40,8 @@ import { MatTabsModule } from '@angular/material/tabs';
         MatSidenavModule,
         MatToolbarModule,
         MatListModule,
-        MatTabsModule
+        MatTabsModule,
+        MatCardModule
     ]
 })
 export class MaterialModule{}
